0d1d09a1d8b238b93235ec7cc99dd374ee52e583
SandipDeb <sandip.deb@xopuntech.com>
2022-05-01 19:51:28 +0530

    initial project setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
+import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
+import "./App.css";
+
+//TODO API KEY : 3c315337
+
+const API_URL = "http://www.omdbapi.com?apikey=3c315337"; // FIXME
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    searchMovies("batman");
+  }, []);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search);
+    console.log(data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>MoviePedia</h1>
+      <div className="search">
+        <input
+          placeholder="Search your favourite Movies"
+          value={searchTerm}
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie, i) => (
+            <MovieCard movie={movie} key={i} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>Sorry🙁! No movies found</h2>
+        </div>
+      )}
+
+      <div className="footer">
+        <p>Coded by Sandip Deb</p>
+      </div>
     </div>
   );
-}
+};
 
 export default App;

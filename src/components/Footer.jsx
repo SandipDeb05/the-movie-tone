0c51e5c8aa60af712d9aa8d5e0fe46f8d1b8cc23
SandipDeb <sandip.deb@xopuntech.com>
2022-05-09 09:32:31 +0530

    footer links added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,30 @@
 import React from "react";
 import { BsLinkedin, BsGithub } from "react-icons/bs";
+
 import "../App.css";
 
 const Footer = () => {
   return (
     <div className="footer">
-      <p>Coded by Sandip Deb </p>
+      <p>
+        Coded by{" "}
+        <a
+          href="https://sandipdeb.netlify.app/"
+          className="footer-link"
+          target={"_blank"}
+        >
+          Sandip Deb
+        </a>{" "}
+        || using{" "}
+        <a
+          href="https://www.omdbapi.com/"
+          target={"_blank"}
+          className="footer-link"
+        >
+          ODMB API
+        </a>
+        .
+      </p>
       <a
         href="https://www.linkedin.com/in/sandip-deb-8b76b2157/"
         target={"_blank"}
